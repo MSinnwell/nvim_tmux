5c064dd63118758df49283d35ffbb0bc08211f52
MSinnwell <marvinsinnwell@gmail.com>
2025-03-12 11:35:24 +0100

    added glow

--- nvim/init.lua
+++ nvim/init.lua
@@ -229,6 +229,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
   {
     'Aasim-A/scrollEOF.nvim',
     event = { 'CursorMoved', 'WinScrolled' },
