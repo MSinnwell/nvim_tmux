68aea08a50dd6b98010a4ca55896c1dbe91eb349
MSinnwell <marvinsinnwell@gmail.com>
2025-10-18 19:03:07 +0200

    added vimtex

--- nvim/init.lua
+++ nvim/init.lua
@@ -277,6 +277,15 @@ require('lazy').setup({
     end,
   },
   {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
+  {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
     event = 'InsertEnter', -- Lazy-load on insert mode
