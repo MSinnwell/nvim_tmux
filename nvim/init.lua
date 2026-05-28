78e734563bda44f9578c0a5ccf75b7d48ac8c76a
Marvin Sinnwell <marvinsinnwell@gmail.com>
2026-05-29 01:29:55 +0200

    diagnostic jump

--- nvim/init.lua
+++ nvim/init.lua
@@ -201,6 +201,13 @@ vim.keymap.set('n', '<leader>nf', ':NvimTreeFocus<CR>', { noremap = true, silent
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '.d', function()
+  vim.diagnostic.jump { count = 1, float = true }
+end, { desc = 'Next Diagnostic' })
+
+vim.keymap.set('n', ',d', function()
+  vim.diagnostic.jump { count = -1, float = true }
+end, { desc = 'Previous Diagnostic' })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
